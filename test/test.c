15e52e53b3c9d2c3c03e92c3eb0d4d6526172d62
amd_6700s <11111>
2019-03-05 23:44:56 +0800

    5号第一次

---
+++ test/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int age[6][4];
+	for (int i = 0; i < 6; i++)
+	{
+		//数组的每一行的地址可以用变量名+i来表示,因为变量名就代表数组第一个值的地址
+		//"age+1"代表第二行第一个字符,"age+2"代表第三行第一个变量地址...
+		//这时age = &age[0][0]
+		printf("age + %d address is %p\n", i, age + i);
+	}
+	for ( int i = 0; i < 6; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			//除了第一个字符的特例外,其他的都需要正常书写
+			printf("age[%d][0] + %d address is %p\n",i,j,&age[i][0]+j);
+		}
+	}
+}
